e961be580842ab4fb9e3aec4c94fe51c3e6a2deb
moggito <90263325+moggichita@users.noreply.github.com>
2023-05-09 17:06:51 +0200

    idk

--- js/settings.js
+++ js/settings.js
@@ -9,6 +9,7 @@ let buttonMouse, buttonKeyboard;
 // methods for each phase of the state
 function preloadAssets() {
     game.load.image("bg","assets/imgs/bg.jpg");
+    game.load.image("boton","assets/imgs/start.png")
 }
 
 function initGame() {
@@ -18,8 +19,8 @@ function initGame() {
     game.add.image(0,0,"bg");
 
     //settingsText = game.add.text(160,30, "SETTINGS", {fontSize: "35px", fill: "#000"});ç
-    buttonMouse = game.add.button(160, 70, onButtonMouse);
-    buttonKeyboard = game.add.button(160, 90, onButtonKeyboard);
+    buttonMouse = game.add.button(160, 100, "boton", onButtonMouse);
+    buttonKeyboard = game.add.button(500, 100, "boton", onButtonKeyboard);
 
     /*Keyboard callback ESTO HABRÁ QUE PASARSELO A LA MAIN ESCENA IGUAL QUE LO DE KEYBOARD INPUT
     game.input.keyboard.onDownCallback = getKeyboardInput;
