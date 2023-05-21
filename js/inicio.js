4b7767f43c8d16b8f70332dba3f10ceef26b17e4
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2023-05-21 15:44:57 +0200

    Merge branch 'main' of https://github.com/DaniPerezUJI/save-the-moon

--- js/inicio.js
+++ js/inicio.js
@@ -4,38 +4,35 @@ let initialState = {
     update: updateGame,
 };
 
-let btnStart, btnInfo, btnCredits;
-let GAP = 120;
+let btnStart, btnConf, btnCredits;
 let TV, BG;
 
 function loadAssets(){
     
-    game.load.spritesheet("start","assets/imgs/START SCREEN/Startspritesheet.png", {frameWidth:925, frameHeight:519});
-    game.load.image("info","assets/imgs/info.png");
-    game.load.image("credits","assets/imgs/credits.png");
+    game.load.spritesheet("start","assets/imgs/START SCREEN/Startspritesheet.png", 925,519);
+    game.load.spritesheet("conf","assets/imgs/START SCREEN/ConfSpritesheet.png",519,519);
+    game.load.spritesheet("credits","assets/imgs/START SCREEN/InfoSpritesheet.png",519,519);
     game.load.image("tele","assets/imgs/BG-1.png");
-    game.load.image("bg","assets/imgs/BG.png");
+    game.load.image("bg","assets/imgs/START SCREEN/BG-MAIN.png");
 }
 
 function displayScreen(){
 
     
     game.input.enabled = true;
-    BG = game.add.image(0,0,"bg")
-    TV = game.add.image(0,0,"tele")
+    BG = game.add.image(0, 0, "bg")
+    TV = game.add.image(0, 0, "tele")
     BG.scale.setTo(game.width/BG.width, game.height/BG.height)
     TV.scale.setTo(game.width/TV.width, game.height/TV.height)
 
-    btnStart = game.add.sprite(200, 200,
-    'start',1);
+    btnStart = game.add.button(200, 200, "start", onStartButtonPressed, 0, 1, 0, 1)
+    btnStart.scale.setTo(0.5, 0.5);
 
-   
-    // btnStart.scale.setTo(0.5, 0.5);
-
-    btnInfo = game.add.button(game.world.width / 1.75, game.world.height / 3 + GAP,
-    'info', onInfoButtonPressed);
-    btnCredits = game.add.button(game.world.width / 1.75, game.world.height / 3 + 2*GAP,
-    'credits', onCreditsButtonPressed);
+    btnConf = game.add.button(270, 420, "conf", onConfButtonPressed, 0, 1, 0, 1)
+    btnConf.scale.setTo(0.22, 0.22);
+    
+    btnCredits = game.add.button(430, 420, "credits", onCreditsButtonPressed, 0, 1, 0, 1)
+    btnCredits.scale.setTo(0.22, 0.22);
 
    
 
@@ -47,7 +44,7 @@ function onStartButtonPressed(){
  
 }
 
-function onInfoButtonPressed(){
+function onConfButtonPressed(){
     game.state.start("settings");
 }
 
