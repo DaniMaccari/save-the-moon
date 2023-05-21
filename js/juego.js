56a52421bde2b234e056683e36e6eb95e3184019
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2023-05-21 15:57:06 +0200

    update

--- js/juego.js
+++ js/juego.js
@@ -10,22 +10,32 @@ function loadAssets(){
 
     game.load.image("levelA", "assets/imgs/start.png");//cambiar la ruta de las imagenes levels A B C
     game.load.image("levelB", "assets/imgs/start.png");//cambiar la ruta de las imagenes levels A B C
-    game.load.image("bg","assets/imgs/bg.jpg");
+
+    game.load.image("tele","assets/imgs/BG-1.png");
+    game.load.image("bg","assets/imgs/START SCREEN/BG-MAIN.png");
     
 };
 
 function displayScreen(){
     
     game.input.enabled = true;
-    game.add.image(0,0,"bg");
+
+    //set bacground
+    BG = game.add.image(0, 0, "bg")
+    BG.scale.setTo(game.width/BG.width, game.height/BG.height)
+    
 
     btnLevelA = game.add.button(game.world.width / 2, (game.world.height / 6) * 2, 'levelA', onLevelAPressed);
     btnLevelB = game.add.button(game.world.width / 2, (game.world.height / 6) * 3, 'levelB', onLevelBPressed);
     btnLevelC = game.add.button(game.world.width / 2, (game.world.height / 6) * 4, 'levelA', onLevelCPressed);
 
+    //TV layout
+    TV = game.add.image(0, 0, "tele")
+    TV.scale.setTo(game.width/TV.width, game.height/TV.height)
 };
 
 function onLevelAPressed(){
+    console.log("level A les GOO")
     game.state.start("levelA");
 };
 function onLevelBPressed(){
