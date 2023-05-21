7e4a23d6a77f12a7ae69793bbe8175e917e391fa
moggito <90263325+moggichita@users.noreply.github.com>
2023-05-21 17:56:20 +0200

    settings

--- js/settings.js
+++ js/settings.js
@@ -5,17 +5,17 @@ let settingsState = {
 
 let settingsText;
 let buttonMouse, buttonKeyboard;
-
+let arrThreads = []
+let isKeyboradActive = true, isMouseActive = false;
 
 
 // methods for each phase of the state
 function preloadAssets() { 
     game.load.image("tele","assets/imgs/BG-1.png");
     game.load.image("bg","/assets/SETTINGS SCREEN/settingsBG.png");
-    game.load.image("thread_on","/assets/SETTINGS SCREEN/thread_on.png");
-    game.load.image("thread_off","/assets/SETTINGS SCREEN/thread_off.png");
     game.load.spritesheet("keyboard","assets/SETTINGS SCREEN/keyboardSpritesheet.png",519,519);
     game.load.spritesheet("mouse","assets/SETTINGS SCREEN/mouseSpritesheet.png",519,519);
+    game.load.spritesheet("threads","assets/SETTINGS SCREEN/threadsSpritesheet.png",182,519);
 }
 
 function initGame() {
@@ -36,22 +36,44 @@ function initGame() {
     game.input.enabled = true;
 
     //THREAD SELECTOR
-    
+    let initialThreadPosX = 200;
+    let threadButton;
+    threadButton.scale.setTo(0.2, 0.2);
+
+    for (let i=0; i<9; i++) {
+        threadButton = game.add.button(initialThreadPosX,500,"threads", onButtonThread(i), 0,0,0,0);
+        arrThreads.push(threadButton);
+        initialThreadPosX += threadButton.width;
+    }
 }
 
-    
+
 function onButtonMouse() {
 
-
-    this.game.global.isKeyboradActive = false;
-    this.game.global.isMouseActive = true;
+    buttonMouse.setFrames(1,1,1,1);
+    buttonKeyboard.setFrames(1,0,0,0);
+    isKeyboradActive = false;
+    isMouseActive = true;
     game.state.start("inicio");
 }
 
 
 function onButtonKeyboard() {
-    this.game.global.isKeyboradActive = true
-    this.game.global.isMouseActive = false
+
+    buttonKeyboard.setFrames(1,1,1,1);
+    buttonMouse.setFrames(1,0,0,0);
+    isKeyboradActive = true
+    isMouseActive = false
     game.state.start("inicio");
 }
     
+function onButtonThread(button) {
+    for (let i= 0; i<9; i++) {
+        if (i<= button) {
+            button.setFrames(1,1,1,1);
+        }
+        else {
+            button.setFrames(0,0,0,0);
+        }
+    }
+}
