562b1fc734b76bbbe0d73666cebfc70a37dcfd0f
moggito <90263325+moggichita@users.noreply.github.com>
2023-05-21 12:28:00 +0200

    Inicio updated assets

--- js/inicio.js
+++ js/inicio.js
@@ -11,24 +11,17 @@ function loadAssets(){
     game.load.image("start","assets/imgs/start.png");
     game.load.image("info","assets/imgs/info.png");
     game.load.image("credits","assets/imgs/credits.png");
-    game.load.image("bg","assets/imgs/bg.jpg");
-    
+    game.load.image("tele","assets/imgs/BG-1.png");
+    game.load.image("bg","assets/imgs/BG.png");
 }
 
 function displayScreen(){
 
     game.input.enabled = true;
-    game.add.image(0,0,"bg")
-
-    let textTitle = 'Save The Moon';
-    let styleTitle = {
-
-        fontSize: '32pt',
-        fontWeight: 'bold',
-        fill: '#b60404'
-    };
-
-    game.add.text(50, game.world.height / 6, textTitle, styleTitle);
+    BG = game.add.image(0,0,"bg")
+    TV = game.add.image(0,0,"tele")
+    BG.scale.setTo(game.width/BG.width, game.height/BG.height)
+    TV.scale.setTo(game.width/TV.width, game.height/TV.height)
 
     btnStart = game.add.button(game.world.width / 1.75, game.world.height / 3,
     'start', onStartButtonPressed);
