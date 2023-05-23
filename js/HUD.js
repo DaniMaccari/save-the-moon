df6872c38cc493bcd0b6098408c1d67c54150869
moggito <90263325+moggichita@users.noreply.github.com>
2023-05-23 15:26:15 +0200

    HUD pull

--- js/HUD.js
+++ js/HUD.js
@@ -9,7 +9,7 @@ function createHUD() {
 
     let styleHUD = { fontSize: "18px", fill: "#FFFFFF" };
 
-    timerText = game.add.text(50,  20, "Time: ", styleHUD);
+    timeText = game.add.text(50,  20, "Time: ", styleHUD);
 
     scoreText = game.add.text(50,  game.world.height/15, "Score: " + score, styleHUD);
 
@@ -28,25 +28,7 @@ function updateScore() {
 function updateTimer() {
     var elapsed = game.time.now - startTime;
     var actualTime = Math.floor(elapsed / 1000);
-    timerText.text = "Time: " + actualTime; // Display in seconds
+    timeText.text = "Time: " + actualTime; // Display in seconds
     
 }
 
-function reciboDaño(lives)
-{
-    if(lives > 1)
-    {
-        lives-=1;
-        
-        livesText.text = "Lives: "+ lives;
-        console.log("no te pilles baby")
-    }
-
-    else
-    {
-        //player.kill();
-        
-        console.log("He muerto")
-    }
-
-}
