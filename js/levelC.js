09e8e45f6f2b146007806d9a48a54de7b409d0b8
Carlos Benedito Tabares <al394807@uji.es>
2023-05-30 10:42:41 +0200

    los sonidos habían roto b y c sin querer jiji

--- js/levelC.js
+++ js/levelC.js
@@ -20,6 +20,11 @@ function loadAssets() {
     game.load.spritesheet("balas","assets/imgs/balasSpritesheet.png",519,519);
     game.load.image("pantallaNegra","assets/imgs/Solid_black.png")
 
+    game.load.audio("piumSound","assets/snds/disparo.mp3");
+    game.load.audio("colisionSound","assets/snds/colision.mp3");
+    game.load.audio("tocadoSound","assets/snds/tocado.mp3");
+    game.load.audio("cambioSound","assets/snds/cambio.mp3")
+
 
 }
 
@@ -44,6 +49,15 @@ function displayScreen() {
     level = 1;
     lives = 5;
     startTime = game.time.now;
+
+    piumSound = game.sound.add("piumSound");
+    piumSound.volume = 0.5;
+    colisionSound = game.sound.add("colisionSound");
+    colisionSound.volume = 0.7;
+    tocadoSound = game.sound.add("tocadoSound");
+    tocadoSound.volume = 0.5;
+    cambioSound = game.sound.add("cambioSound");
+    cambioSound.volume = 0.5;
     
 
     timerLifeItems = game.time.create(false);
